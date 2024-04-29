e9ea858ec535f44235c4d624b19d21b01b329b72
Debora Tayna <dtayna.3194@gmail.com>
2024-04-29 14:26:11 -0300

    tipos organizados

--- typesdt.hs
+++ typesdt.hs
@@ -1,23 +1,15 @@
-data Llong = Llong Integer
-
-instance Show Llong where
-    show (Llong x) = "long " ++ show x
-
-data Lfloat = Lfloat Float
-
-data Ldouble = Ldouble Double
-
-data Lbool = Lbool Bool
-
-data Lchar = Lchar Char
-
-data Lstring = Lstring String
-
--- Jeito que funcionou os tipos recursivos:
-
-data Type = Ltuple Type [Type] |  Lint Int
+data Type = Tuple Type [Type] |  Int Int | Long Integer | Float Float | Double Double | Bool Bool | Char Char | String String
 
 instance Show Type where
-    show (Ltuple x (y:ys)) = show x ++ "," ++ show (Ltuple y ys)
-    show (Ltuple x []) = show x
-    show (Lint x) = show x
+    show (Tuple x (y:ys)) = show x  ++ "," ++ show (Tuple y ys)
+    show (Tuple x []) = show x
+    show (Int x) = show x
+    show (Long x) = show x
+    show (Float x) = show x
+    show (Double x) = show x
+    show (Bool x) = show x
+    show (Char x) = show x
+    show (String x) = show x
+
+   
+ 
