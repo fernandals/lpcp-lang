0e4f40e0b2d429a662a983a4dafe26ea14f8b7f4
lucasbazante <lucasbazante1@gmail.com>
2024-06-19 17:00:34 -0300

    supposedly it works

--- src/State.hs
+++ src/State.hs
@@ -5,47 +5,79 @@ module State where
 import Lexer
 import Utils
 
--- modifier, type, id, val
-type StateEntry = (Token, Token, Token, Token)
+-- Wether it should execute or just parse
+type ExecutionFlag = Bool
 
-type StateQuery = (Token, Token)
+-- An entry to a variable in the table contains its modifier, type and value.
+-- The symbol table contains a list of its versions, so it can support recursion.
+-- The top of the stack of activations contains the most recent version of it, its executing version.
+type SymbolEntry = (Token, Token, Token)
 
-type State = [StateEntry]
+type Symbol = (String, [SymbolEntry])
 
-stateInsert :: StateEntry -> State -> State
-stateInsert entry [] = [entry]
-stateInsert entry@(mod, t, id, val) (sym@(_, _, id', val') : σ)
-  | id == id' = entry : σ
-  | otherwise = sym : stateInsert entry σ
+type SymTable = [Symbol]
 
-stateUpdate :: StateQuery -> State -> State
-stateUpdate _ [] = error "No variable with given name."
-stateUpdate query@(id, val) (sym@(mod', t', id', val') : σ)
-  | id == id' = case mod' of
+-- And activation stack entry just contains the name of the function that is executing in the given moment,
+-- plus the depth of its activation, i.e. if its a recursive function, it'll call itself many times and this
+-- will increase its depth.
+-- The activation stack just stacks up subprograms activations.
+type ActEntry = (String, Int)
+
+type ActStack = [ActEntry]
+
+-- An entry to the scope table contains the id of the entry and the id of its static parent.
+-- To access things by scope, we just need to follow the static chain from our current id to all its static parents.
+-- The global scope is an ancestor of everyone
+type ScopeEntry = (String, String)
+
+type ScopeTable = [ScopeEntry]
+
+type TypeEntry = String
+
+type TypeTable = [TypeEntry]
+
+type SubpEntry = String
+
+type SubpTable = [SubpEntry]
+
+type State = (ExecutionFlag, SymTable, ActStack, ScopeTable, TypeTable, SubpTable)
+
+getSymTable :: State -> SymTable
+getSymTable (_, symt, _, _, _, _) = symt
+
+putSymTable :: State -> SymTable -> State
+putSymTable (flag, _, stack, scope, types, subp) symt = (flag, symt, stack, scope, types, subp)
+
+symTableInsert :: String -> SymbolEntry -> SymTable -> SymTable
+symTableInsert name entry [] = [(name, [entry])]
+symTableInsert name entry (sym@(name', entry') : symt)
+  | name == name' = error "."
+  | otherwise = sym : symTableInsert name entry symt
+
+symTableUpdate :: String -> Token -> SymTable -> SymTable
+symTableUpdate _ _ [] = error "No variable with given name.\n"
+symTableUpdate name val (sym@(name', (mod, t, val') : entry) : symt)
+  | name == name' = case mod of
       Mut _ ->
-        if typeof val /= typeof t'
+        if typeof val /= typeof t
           then typerror
-          else (mod', t', id', val) : σ
-      Let _ -> error "You can't change an immutable value."
-  | otherwise = sym : stateUpdate query σ
+          else (name', (mod, t, val) : entry) : symt
+      Let _ -> error "You can't change an immutable variable.\n"
+  | otherwise = sym : symTableUpdate name val symt
   where
     typerror =
       error $
         "Type mismatch at "
-          ++ show (pos id)
+          ++ show (pos val)
           ++ ".\n"
           ++ "Expected "
-          ++ typeof t'
+          ++ typeof t
           ++ ", got "
           ++ typeof val
           ++ ".\n"
 
-stateDelete :: StateQuery -> State -> State
-stateDelete _ [] = error "No variable with given name."
-stateDelete query@(id, val) (sym@(_, _, id', val') : σ)
-  | id == id' = σ
-  | otherwise = sym : stateDelete query σ
-
-getValue :: Token -> State -> Token
-getValue _ [] = error "No variable with given name."
-getValue id (sym@(mod', t', id', val') : σ) = if id == id' then val' else getValue id σ
+symTableGetVal :: String -> SymTable -> Token
+symTableGetVal _ [] = error "No variable with given name."
+symTableGetVal name ((name', (_, _, val) : _) : symt)
+  | name == name' = val
+  | otherwise = symTableGetVal name symt
